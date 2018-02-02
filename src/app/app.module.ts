dd9e1741e3238aea52111b3af5527b4692c2665f
Vishakha Sehgal <sehgalv@vcu.edu>
2018-02-02 01:05:43 -0500

    basic navbar with tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { RouterModule, Routes} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { InvolvementComponent } from './involvement/involvement.component';
+import { AcademicsComponent } from './academics/academics.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
 
+const appRoutes: Routes= [
+  {path: '', component: HomeComponent},
+  {path: 'academics', component: AcademicsComponent},
+  {path: 'involvement', component: InvolvementComponent},
+  {path: 'hobbies', component: HobbiesComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    InvolvementComponent,
+    AcademicsComponent,
+    HobbiesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
